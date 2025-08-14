e7969d20a7d4df0142fc66e0e5b9d71fb879300f
Adrian Marghidan <mihail.adrian.marghidan@ibm.com>
2025-08-14 14:13:50 +0300

    feat: Add failing test for new endpoint

--- app.test.js
+++ app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-    it('should respond with "Hello, CI/CD World!"', async () => {
-        const response = await request(app).get('/');
+    it('should respond to a new endpoint', async () => {
+        const response = await request(app).get('/new');
         expect(response.statusCode).toBe(200);
-        expect(response.text).toBe('Hello, CI/CD World!');
+        expect(response.text).toBe('Hello, New Endpoint!');
     });
-});
+})
